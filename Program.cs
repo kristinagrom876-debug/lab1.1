60daf821155807f0d9bee50df11e0694ea88d02d
kristina <kristinagrom876@gmail.com>
2026-02-10 19:21:31 +0700

    переделала

--- Program.cs
+++ Program.cs
@@ -13,12 +13,11 @@ class Program {
     digitsToRemoveCount = 1;
 
     while (true) {
-      Console.Write("Select Task:\n1 - Exponentiation\n2 - Second Digit Permutation\n3 - Exit\nYour Choice:");
+      Console.Write("\nSelect Task: \n1 - Exponentiation\n2 - Second Digit Permutation\n3 - Exit\nYour Choice: ");
 
       userChoice = Console.ReadLine();
 
-      if (userChoice == "1")
-      {
+      if (userChoice == "1") {
         Console.Write("a: ");
         baseNumber = int.Parse(Console.ReadLine());
 
@@ -26,20 +25,17 @@ class Program {
         exponentValue = int.Parse(Console.ReadLine());
 
         calculationResult = powerInitialValue;
-        for (loopCounter = 0; loopCounter < exponentValue; ++loopCounter)
-        {
+        for (loopCounter = 0; loopCounter < exponentValue; ++loopCounter) {
           calculationResult *= baseNumber;
         }
 
         Console.WriteLine($"Calculation Result: {calculationResult}");
       }
-      else if (userChoice == "2")
-      {
+      else if (userChoice == "2") {
         Console.Write("x: ");
         inputNumber = Console.ReadLine();
 
-        if (inputNumber.Length <= numberLength)
-        {
+        if (inputNumber.Length <= numberLength) {
           Console.WriteLine("Number Must Contain At Least 3 Digits!");
           continue;
         }
